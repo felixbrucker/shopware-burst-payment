2677ce5e5a79bddd8b2b368add0c53ebc7608973
Felix Brucker <felix@felixbrucker.com>
2020-05-16 14:03:45 +0200

    Add plugin config link to settings

---
+++ src/Resources/app/administration/src/component/sw-settings-index/index.js
@@ -0,0 +1,17 @@
+import template from './sw-settings-index.html.twig';
+
+Shopware.Component.override('sw-settings-index', {
+    template,
+
+    data() {
+        return {
+            pluginIconPath: 'bundles/burstpayment/static/img/plugin.png',
+        };
+    },
+});
+
+
+
+
+
+
--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,2 +1,3 @@
 import './translations/init';
 import './component/sw-order-user-card';
+import './component/sw-settings-index';
