264c8699f36146646b2a9132a5984986445f395a
Felix Brucker <felix@felixbrucker.com>
2020-05-17 20:25:45 +0200

    Debug

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -32,7 +32,7 @@ cd "$SHOPWARE_DEVELOPMENT_DIR" && ./psh.phar administration:build
 echo "Building the release zip"
 PLUGIN_DIST_DIR="$TMP_DIR/burst-payment"
 cd /
-mv "$PLUGIN_DIR" "$PLUGIN_DIST_DIR"
+cp -R "$PLUGIN_DIR" "$PLUGIN_DIST_DIR"
 cd "$TMP_DIR"
 zip -r "burst-payment-$TAG.zip" "burst-payment" --exclude burst-payment/node_modules/\* bin burst-payment/test/\* burst-payment/.editorconfig burst-payment/.gitignore burst-payment/coverage\*.xml burst-payment/phpunit.xml burst-payment/.git/\* burst-payment/.github/\*
-cp "burst-payment-$TAG.zip" "$PLUGIN_DIR/"
+cp "burst-payment-$TAG.zip" "$PLUGIN_DIR/burst-payment-$TAG.zip"
