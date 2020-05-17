c5ff4accbaeb51d59b3578a9751f025a1f5e2be1
Felix Brucker <felix@felixbrucker.com>
2020-05-17 19:38:25 +0200

    Debug

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -6,6 +6,7 @@ TAG="${GITHUB_REF##*/}"
 TAG="${TAG:-dev}"
 echo "Building v$TAG with Shopware v$SHOPWARE_VERSION"
 
+DB_PORT="${DB_PORT:-3306}"
 PLUGIN_DIR="$(pwd)"
 TMP_DIR="$(mktemp -d)"
 SHOPWARE_DEVELOPMENT_DIR="$TMP_DIR/shopware-development"
@@ -17,8 +18,8 @@ cd "$PLUGIN_DIR" && npm install
 echo "Installing Shopware v$SHOPWARE_VERSION"
 git clone --single-branch --branch "$SHOPWARE_VERSION" https://github.com/shopware/development.git "$SHOPWARE_DEVELOPMENT_DIR"
 ln -s "$PLUGIN_DIR" "$SHOPWARE_DEVELOPMENT_DIR/custom/plugins/BurstPayment"
-printf "const:\n    APP_ENV: \"dev\"\n    APP_URL: \"http://127.0.0.1\"\n    DB_HOST: \"127.0.0.1\"\n    DB_PORT: \"3306\"\n    DB_NAME: \"shopware6release\"\n    DB_USER: \"root\"\n    DB_PASSWORD: \"shopware\"" > "$SHOPWARE_DEVELOPMENT_DIR/.psh.yaml.override"
-cd "$SHOPWARE_DEVELOPMENT_DIR" && ./psh.phar install --DB_HOST="127.0.0.1" --DB_USER="root" --DB_PASSWORD="shopware" --DB_PORT="3306"
+#printf "const:\n    APP_ENV: \"dev\"\n    APP_URL: \"http://127.0.0.1\"\n    DB_HOST: \"127.0.0.1\"\n    DB_PORT: \"3306\"\n    DB_NAME: \"shopware6release\"\n    DB_USER: \"root\"\n    DB_PASSWORD: \"shopware\"" > "$SHOPWARE_DEVELOPMENT_DIR/.psh.yaml.override"
+cd "$SHOPWARE_DEVELOPMENT_DIR" && ./psh.phar install --DB_HOST="127.0.0.1" --DB_USER="shopware" --DB_PASSWORD="shopware" --DB_PORT="$DB_PORT"
 cd "$SHOPWARE_DEVELOPMENT_DIR" && bin/console plugin:install --activate BurstPayment
 
 echo "Building the administration bundle"
