36b295743f76245b81117ae623a0d16c0edd175f
Felix Brucker <felix@felixbrucker.com>
2020-05-17 19:52:51 +0200

    Debug

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -8,10 +8,12 @@ echo "Building v$TAG with Shopware v$SHOPWARE_VERSION"
 
 DB_PORT="${DB_PORT:-3306}"
 PLUGIN_DIR="$(pwd)"
+ls -lah "$PLUGIN_DIR"
 TMP_DIR="$(mktemp -d)"
 SHOPWARE_DEVELOPMENT_DIR="$TMP_DIR/shopware-development"
 
 echo "Installing plugin dependencies"
+cd "$PLUGIN_DIR" && composer install
 cd "$PLUGIN_DIR/autoload-dist" && composer install
 cd "$PLUGIN_DIR" && npm install
 
@@ -19,7 +21,10 @@ echo "Installing Shopware v$SHOPWARE_VERSION"
 git clone --single-branch --branch "$SHOPWARE_VERSION" https://github.com/shopware/development.git "$SHOPWARE_DEVELOPMENT_DIR"
 ln -s "$PLUGIN_DIR" "$SHOPWARE_DEVELOPMENT_DIR/custom/plugins/BurstPayment"
 printf "const:\n    APP_ENV: \"dev\"\n    APP_URL: \"http://127.0.0.1\"\n    DB_HOST: \"127.0.0.1\"\n    DB_PORT: \"$DB_PORT\"\n    DB_NAME: \"shopware6release\"\n    DB_USER: \"root\"\n    DB_PASSWORD: \"shopware\"" > "$SHOPWARE_DEVELOPMENT_DIR/.psh.yaml.override"
-cd "$SHOPWARE_DEVELOPMENT_DIR" && ./psh.phar install
+cd "$SHOPWARE_DEVELOPMENT_DIR" && ./psh.phar init-composer
+cd "$SHOPWARE_DEVELOPMENT_DIR" && ./psh.phar init-database
+cd "$SHOPWARE_DEVELOPMENT_DIR" && ./psh.phar init-shopware
+cd "$SHOPWARE_DEVELOPMENT_DIR" && ./psh.phar administration:install-dependencies
 cd "$SHOPWARE_DEVELOPMENT_DIR" && bin/console plugin:install --activate BurstPayment
 
 echo "Building the administration bundle"
@@ -35,3 +40,4 @@ ls -lah "$PLUGIN_DIST_DIR"
 cd "$TMP_DIR"
 zip -r "burst-payment-$TAG.zip" "burst-payment" --exclude burst-payment/vendor/\* burst-payment/node_modules/\* bin burst-payment/test/\* burst-payment/.editorconfig burst-payment/.gitignore burst-payment/coverage\*.xml burst-payment/phpunit.xml burst-payment/.git/\* burst-payment/.github/\*
 cp "burst-payment-$TAG.zip" "$PLUGIN_DIR/"
+exit 1;
