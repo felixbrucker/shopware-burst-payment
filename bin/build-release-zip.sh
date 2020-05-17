2ddb741a7d79a83f8b4842f79fa209f7d3a40213
Felix Brucker <felix@felixbrucker.com>
2020-05-17 21:12:11 +0200

    Debug

--- bin/build-release-zip.sh
+++ bin/build-release-zip.sh
@@ -15,7 +15,5 @@ cd /
 cp -R "$PLUGIN_DIR" "$PLUGIN_DIST_DIR"
 cd "$TMP_DIR"
 PLUGIN_ZIP_NAME="$PLUGIN_NAME_PATH-$TAG.zip"
-zip -r "$PLUGIN_ZIP_NAME" "$PLUGIN_NAME_PATH" --exclude burst-payment/node_modules/\* bin burst-payment/test/\* burst-payment/.editorconfig burst-payment/.gitignore burst-payment/coverage\*.xml burst-payment/phpunit.xml burst-payment/.git/\* burst-payment/.github/\*
+zip -r "$PLUGIN_ZIP_NAME" "$PLUGIN_NAME_PATH" --exclude burst-payment/node_modules/\* burst-payment/bin/\* burst-payment/test/\* burst-payment/.editorconfig burst-payment/.gitignore burst-payment/coverage\*.xml burst-payment/phpunit.xml burst-payment/.git/\* burst-payment/.github/\*
 cp "$PLUGIN_ZIP_NAME" "$PLUGIN_DIR/$PLUGIN_ZIP_NAME"
-cd "$PLUGIN_DIR"
-ls -lah "$PLUGIN_DIR"
