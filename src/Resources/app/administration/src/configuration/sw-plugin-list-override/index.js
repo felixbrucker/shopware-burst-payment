d64c39bd2a041356e0e901d3df2c3631623f2fef
Felix Brucker <felix@felixbrucker.com>
2020-05-18 22:08:01 +0200

    Fix config redirect from plugin manager

--- src/Resources/app/administration/src/configuration/sw-plugin-list-override/index.js
+++ src/Resources/app/administration/src/configuration/sw-plugin-list-override/index.js
@@ -2,4 +2,10 @@ import template from './sw-plugin-list.html.twig';
 
 Shopware.Component.override('sw-plugin-list', {
     template,
+
+    methods: {
+        openBurstPaymentPluginConfig() {
+            this.$router.push({ name: 'burst-payment.settings' });
+        },
+    }
 });
