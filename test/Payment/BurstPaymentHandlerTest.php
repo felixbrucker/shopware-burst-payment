35047efb095acf3d67adb90a0cb465fb9f899b44
Felix Brucker <felix@felixbrucker.com>
2020-05-20 23:09:47 +0200

    Moar tests

--- test/Payment/BurstPaymentHandlerTest.php
+++ test/Payment/BurstPaymentHandlerTest.php
@@ -2,17 +2,24 @@
 
 namespace Burst\BurstPayment\Test\Payment;
 
+use Burst\BurstPayment\BurstRate\BurstRateEntity;
 use Burst\BurstPayment\BurstRate\BurstRateService;
 use Burst\BurstPayment\Payment\BurstPaymentHandler;
 use Burst\BurstPayment\Services\OrderTransactionService;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Checkout\Cart\Exception\CustomerNotLoggedInException;
+use Shopware\Core\Checkout\Cart\Price\Struct\CalculatedPrice;
+use Shopware\Core\Checkout\Cart\Tax\Struct\CalculatedTaxCollection;
+use Shopware\Core\Checkout\Cart\Tax\Struct\TaxRuleCollection;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Checkout\Payment\Cart\SyncPaymentTransactionStruct;
+use Shopware\Core\Checkout\Payment\Exception\SyncPaymentProcessException;
+use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\Currency\CurrencyEntity;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
 /**
@@ -26,19 +33,9 @@ class BurstPaymentHandlerTest extends TestCase
     private $orderTransactionServiceMock;
 
     /**
-     * @var BurstRateService|MockObject
-     */
-    private $burstRateServiceMock;
-
-    /**
      * @var BurstPaymentHandler
      */
     private $burstPaymentHandler;
-
-    /**
-     * @var OrderEntity
-     */
-    private $orderEntity;
 
     /**
      * @var OrderTransactionEntity
@@ -66,24 +63,50 @@ class BurstPaymentHandlerTest extends TestCase
     private $customerEntity;
 
     /**
+     * @var BurstRateEntity
+     */
+    private $burstRate;
+
+    /**
+     * @var CurrencyEntity
+     */
+    private $currency;
+
+    /**
+     * @var Context
+     */
+    private $context;
+
+    /**
+     * @var array
+     */
+    private $paymentContext;
+
+    /**
      * @before
      */
     public function setUpTest(): void
     {
         $this->orderTransactionServiceMock = $this->createMock(OrderTransactionService::class);
-        $this->burstRateServiceMock = $this->createMock(BurstRateService::class);
+        $burstRateServiceMock = $this->createMock(BurstRateService::class);
         $this->burstPaymentHandler = new BurstPaymentHandler(
             $this->orderTransactionServiceMock,
-            $this->burstRateServiceMock
+            $burstRateServiceMock
         );
-        $this->orderEntity = new OrderEntity();
-        $this->orderEntity->setId('1234');
+        $orderEntity = new OrderEntity();
+        $orderEntity->setId('1234');
         $this->orderTransactionEntity = new OrderTransactionEntity();
         $this->orderTransactionEntity->setId('12345');
-        $this->orderTransactionEntity->setOrder($this->orderEntity);
+        $this->orderTransactionEntity->setOrder($orderEntity);
+        $this->orderTransactionEntity->setAmount(new CalculatedPrice(
+            100,
+            100,
+            new CalculatedTaxCollection(),
+            new TaxRuleCollection()
+        ));
         $this->syncPaymentTransactionStruct = new SyncPaymentTransactionStruct(
             $this->orderTransactionEntity,
-            $this->orderEntity
+            $orderEntity
         );
         $this->requestDataBag = new RequestDataBag();
         $this->salesChannelContextMock = $this->createMock(SalesChannelContext::class);
@@ -93,6 +116,27 @@ class BurstPaymentHandlerTest extends TestCase
             ->willReturnCallback(function () {
                 return $this->customerEntity;
             });
+        $this->currency = new CurrencyEntity();
+        $this->currency->setDecimalPrecision(2);
+        $this->currency->setIsoCode('EUR');
+        $this->salesChannelContextMock
+            ->method('getCurrency')
+            ->willReturnCallback(function () {
+                return $this->currency;
+            });
+        $this->context = Context::createDefaultContext();
+        $this->salesChannelContextMock
+            ->method('getContext')
+            ->willReturn($this->context);
+        $this->burstRate = new BurstRateEntity();
+        $this->burstRate->setRate(2);
+        $burstRateServiceMock->method('getBurstRate')->willReturnCallback(function () {
+            return $this->burstRate;
+        });
+        $this->paymentContext = [];
+        $this->orderTransactionServiceMock
+            ->method('getBurstPaymentContext')
+            ->willReturn($this->paymentContext);
     }
 
     /**
@@ -109,5 +153,87 @@ class BurstPaymentHandlerTest extends TestCase
             $this->requestDataBag,
             $this->salesChannelContextMock
         );
+    }
+
+    /**
+     * @testdox throws a SyncPaymentProcessException when paying an order and no burst rate is found for the sales channel currency
+     */
+    public function test_pay_noBurstRateFoundForCurrency(): void
+    {
+        $this->burstRate = null;
+        $this->currency->setIsoCode('XYZ');
+
+        $this->expectExceptionObject(new SyncPaymentProcessException(
+            $this->orderTransactionEntity->getId(),
+            'Currency XYZ can not be converted to BURST'
+        ));
+
+        $this->burstPaymentHandler->pay(
+            $this->syncPaymentTransactionStruct,
+            $this->requestDataBag,
+            $this->salesChannelContextMock
+        );
+    }
+
+    /**
+     * @testdox uses 0 as amount to pay when paying an order and the total price is zero
+     */
+    public function test_pay_totalPriceIsZero(): void
+    {
+        $this->burstRate->setRate(3);
+        $this->orderTransactionEntity->setAmount(new CalculatedPrice(
+            0,
+            0,
+            new CalculatedTaxCollection(),
+            new TaxRuleCollection()
+        ));
+
+        $this->orderTransactionServiceMock
+            ->expects(self::once())
+            ->method('setBurstPaymentContext')
+            ->with(
+                $this->orderTransactionEntity,
+                $this->context,
+                [
+                    'amountToPayInNQT' => '0',
+                    'amountToPayInBurst' => '0.00000000',
+                    'burstRateUsed' => 3,
+                    'transactionState' => 'unmatched',
+                ]
+            );
+
+        $this->burstPaymentHandler->pay(
+            $this->syncPaymentTransactionStruct,
+            $this->requestDataBag,
+            $this->salesChannelContextMock
+        );
+    }
+
+    /**
+     * @testdox adds a very small random amount to the amount to pay to get a unique amount when paying an order
+     */
+    public function test_pay_addUniqueAmountToPrice(): void
+    {
+        $this->burstRate->setRate(1 / 3);
+        $this->orderTransactionEntity->setAmount(new CalculatedPrice(
+            2,
+            2,
+            new CalculatedTaxCollection(),
+            new TaxRuleCollection()
+        ));
+        $this->orderTransactionServiceMock
+            ->method('setBurstPaymentContext')
+            ->willReturnCallback(function ($orderTransaction, $context, $paymentContext) {
+                $this->paymentContext = $paymentContext;
+            });
+
+        $this->burstPaymentHandler->pay(
+            $this->syncPaymentTransactionStruct,
+            $this->requestDataBag,
+            $this->salesChannelContextMock
+        );
+
+        self::assertGreaterThan('6', $this->paymentContext['amountToPayInBurst']);
+        self::assertGreaterThan('600000000', $this->paymentContext['amountToPayInNQT']);
     }
 }
