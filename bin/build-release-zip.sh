99f4e669b9161f796bbca5d4842d94663cb370a0
Felix Brucker <felix@felixbrucker.com>
2020-05-20 19:21:10 +0200

    Fix build release zip

--- bin/build-release-zip.sh
+++ bin/build-release-zip.sh
@@ -2,17 +2,16 @@
 set -e
 
 PLUGIN_NAME="BurstPayment"
-PLUGIN_NAME_PATH="burst-payment"
 
 TAG="${TAG:-dev}"
 echo "Building release zip for $PLUGIN_NAME $TAG"
 
 PLUGIN_DIR="$(pwd)"
 TMP_DIR="$(mktemp -d)"
-PLUGIN_DIST_DIR="$TMP_DIR/$PLUGIN_NAME_PATH"
+PLUGIN_DIST_DIR="$TMP_DIR/$PLUGIN_NAME"
 
 cp -R "$PLUGIN_DIR" "$PLUGIN_DIST_DIR"
 cd "$TMP_DIR"
-PLUGIN_ZIP_NAME="$PLUGIN_NAME_PATH-$TAG.zip"
-zip -r "$PLUGIN_ZIP_NAME" "$PLUGIN_NAME_PATH" --exclude burst-payment/node_modules/\* burst-payment/bin/\* burst-payment/test/\* burst-payment/.editorconfig burst-payment/.gitignore burst-payment/coverage\*.xml burst-payment/phpunit\*.xml burst-payment/.git/\* burst-payment/.github/\*
+PLUGIN_ZIP_NAME="burst-payment-$TAG.zip"
+zip -r "$PLUGIN_ZIP_NAME" "$PLUGIN_NAME" --exclude "$PLUGIN_NAME"/node_modules/\* "$PLUGIN_NAME"/bin/\* "$PLUGIN_NAME"/test/\* "$PLUGIN_NAME"/.editorconfig "$PLUGIN_NAME"/.gitignore "$PLUGIN_NAME"/coverage\*.xml "$PLUGIN_NAME"/phpunit\*.xml "$PLUGIN_NAME"/.git/\* "$PLUGIN_NAME"/.github/\*
 cp "$PLUGIN_ZIP_NAME" "$PLUGIN_DIR/$PLUGIN_ZIP_NAME"
