1e6129b7b078e8c8bcf05f7601a73a5d8a50084a
Felix Brucker <felix@felixbrucker.com>
2020-05-24 20:41:03 +0200

    Remove composer.lock from release zip

--- bin/build-release-zip.sh
+++ bin/build-release-zip.sh
@@ -13,5 +13,5 @@ PLUGIN_DIST_DIR="$TMP_DIR/$PLUGIN_NAME"
 cp -R "$PLUGIN_DIR" "$PLUGIN_DIST_DIR"
 cd "$TMP_DIR"
 PLUGIN_ZIP_NAME="burst-payment-$TAG.zip"
-zip -r "$PLUGIN_ZIP_NAME" "$PLUGIN_NAME" --exclude "$PLUGIN_NAME"/node_modules/\* "$PLUGIN_NAME"/bin/\* "$PLUGIN_NAME"/test/\* "$PLUGIN_NAME"/.editorconfig "$PLUGIN_NAME"/.gitignore "$PLUGIN_NAME"/coverage\*.xml "$PLUGIN_NAME"/phpunit\*.xml "$PLUGIN_NAME"/.git/\* "$PLUGIN_NAME"/.github/\*
+zip -r "$PLUGIN_ZIP_NAME" "$PLUGIN_NAME" --exclude "$PLUGIN_NAME"/node_modules/\* "$PLUGIN_NAME"/bin/\* "$PLUGIN_NAME"/test/\* "$PLUGIN_NAME"/.editorconfig "$PLUGIN_NAME"/.gitignore "$PLUGIN_NAME"/coverage\*.xml "$PLUGIN_NAME"/phpunit\*.xml "$PLUGIN_NAME"/.git/\* "$PLUGIN_NAME"/.github/\* "$PLUGIN_NAME"/composer.lock
 cp "$PLUGIN_ZIP_NAME" "$PLUGIN_DIR/$PLUGIN_ZIP_NAME"
